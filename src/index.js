dfee7a4dc7bc5bf226f70ef6577f5236c5b16e0b
William Gibson <w.gibbo3@icloud.com>
2019-10-17 14:38:50 +0100

    Match code style

--- src/index.js
+++ src/index.js
@@ -11,19 +11,16 @@ console.log(require('fs').readFileSync('logo.txt', 'utf8'))
 const wss = new Server({ port: 9001 }, () => log('WebSocket Server running on :9001'))
 wss.on('connection', async (socket, { url }) => {
     const { t: token } = parse(url)
+
     if(!token) {
         log(`A client tried to connect but didn't provide a token`)
         return socket.close()
     }
 
-    let id
+    let payload
 
     try {
-        id = verify(token, process.env.APERTURE_KEY).id
-        if(!id) {
-            log(`A client was rejected as the portal ID could not be found`)
-            return socket.close()
-        }
+        payload = verify(token, process.env.APERTURE_KEY)
     } catch (error) {
         // I think we should log this for now as some people are experiencing issues
         // with this as their token gets expired, but shouldn't crash aperture now.
@@ -31,7 +28,14 @@ wss.on('connection', async (socket, { url }) => {
         return socket.close()
     }
 
-    const server = await fetchPortalFromId(id)
+    if(!payload.id) {
+        log(`A client was rejected as the portal ID could not be found`)
+        return socket.close()
+    }
+
+    const { id } = payload,
+            server = await fetchPortalFromId(id)
+
     if(!server) {
         log(`A client tried to connect to a stream which does not exists: ${id}`)
         return socket.close()
@@ -54,18 +58,21 @@ const server = http.createServer((req, res) => {
         return res.end(null)
     }
 
-    let id
+    let payload
 
     try {
-        id = verify(token, process.env.APERTURE_KEY).id
-        if(!id) {
-            log(`An attempted stream from ${address} was rejected as the portal ID could not be found`)
-            return res.end(null)
-        }
+        payload = verify(token, process.env.APERTURE_KEY)
     } catch (error) {
         console.error(`An attempted stream from ${address} failed in authentication`, error)
         return res.end(null)
     }
+
+    if(!payload.id) {
+        log(`An attempted stream from ${address} was rejected as the portal ID could not be found`)
+        return res.end(null)
+    }
+
+    const { id } = payload
 
     res.connection.setTimeout(0)
     log(`Stream with ID ${id} connected from ${address}`)
