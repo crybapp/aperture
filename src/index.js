93b208a7632d009d89e4b122bc4d831aa5a5a391
JeDaYoshi <jedayoshi@disroot.org>
2019-10-17 02:48:18 -0400

    Another fix
    I don't know what am I doing anymore, give me some sleep

--- src/index.js
+++ src/index.js
@@ -17,16 +17,21 @@ wss.on('connection', async (socket, { url }) => {
     }
 
     try {
-        const { id } = verify(token, process.env.APERTURE_KEY), server = await fetchPortalFromId(id)
-        if(!id)
+        const { id } = verify(token, process.env.APERTURE_KEY)
+        if(!id) {
             log(`A client was rejected as the portal ID could not be found`)
-        if(!server)
-            log(`A client tried to connect to a stream which does not exists: ${id}`)
-        if(!id || !server) return socket.close()
+            return socket.close()
+        }
     } catch (error) {
         // I think we should log this for now as some people are experiencing issues
         // with this as their token gets expired, but shouldn't crash aperture now.
         console.error(`A client failed to authenticate`, error)
+        return socket.close()
+    }
+
+    const server = await fetchPortalFromId(id)
+    if(!server) {
+        log(`A client tried to connect to a stream which does not exists: ${id}`)
         return socket.close()
     }
 
@@ -53,19 +58,19 @@ const server = http.createServer((req, res) => {
             log(`An attempted stream from ${address} was rejected as the portal ID could not be found`)
             return res.end(null)
         }
+
+        res.connection.setTimeout(0)
+        log(`Stream with ID ${id} connected from ${address}`)
+
+        req.on('data', data =>
+            Array.from(wss.clients)
+                .filter(client => client['id'] === id)
+                .forEach(client => client.send(data))
+        )
     } catch (error) {
         console.error(`An attempted stream from ${address} failed in authentication`, error)
         return res.end(null)
     }
-
-    res.connection.setTimeout(0)
-    log(`Stream with ID ${id} connected from ${address}`)
-    
-    req.on('data', data =>
-        Array.from(wss.clients)
-            .filter(client => client['id'] === id)
-            .forEach(client => client.send(data))
-    )
 })
 
 server.listen(9000, () => log('Streaming Server running on :9000'))
