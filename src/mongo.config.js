45678cbcea9b1f8991c8330813ccab9c4e873a5f
JeDaYoshi <jedayoshi@disroot.org>
2019-10-19 17:08:02 -0400

    Fix mongoose

--- src/mongo.config.js
+++ src/mongo.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
-import { connect, model } from 'mongoose'
+import mongoose from 'mongoose'
 
-connect(process.env.MONGO_URI, { useNewUrlParser: true })
-const collection = model('Portal')
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+const collection = mongoose.model('Portal')
 export default collection
