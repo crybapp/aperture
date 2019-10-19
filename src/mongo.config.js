d7bd88c35cde08a99af76d850815a654f375484b
JeDaYoshi <jedayoshi@disroot.org>
2019-10-19 17:10:22 -0400

    Add Portal schema

--- src/mongo.config.js
+++ src/mongo.config.js
@@ -2,6 +2,20 @@ require('dotenv').config()
 
 import mongoose from 'mongoose'
 
+const ModelSchema = new mongoose.Schema({
+    info: {
+        id: String,
+        createdAt: Number,
+        recievedAt: Number,
+
+        room: String,
+        status: String
+    },
+    data: {
+        serverId: String
+    }
+})
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-const collection = mongoose.model('Portal')
+const collection = mongoose.model('Portal', ModelSchema)
 export default collection
